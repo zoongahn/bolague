256e0d79402c1fda117bfcf13613a36eba9fe1af
joonghyunahn <anjungh0103@gmail.com>
2024-06-09 15:09:11 +0900

    최초 커밋

---
+++ src/main/java/com/zoong/bolague/BolagueApplication.java
@@ -0,0 +1,13 @@
+package com.zoong.bolague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolagueApplication.class, args);
+	}
+
+}
